2bf3742afc7a4073ac09cfd6f37bdc0426892feb
pozhao <po.zhao.huang@gmail.com>
2021-03-26 17:46:37 +0800

    Add HtmlExtensions.

---
+++ CSWeb/App_Code/HtmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace CSWeb.App_Code
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString CreateButton(this HtmlHelper helper, string id, string text)
+        {
+            TagBuilder tag = new TagBuilder("input");
+            tag.Attributes.Add("id", id);
+            tag.Attributes.Add("value", text);
+            tag.Attributes.Add("type", "submit");
+            tag.AddCssClass("btn btn-primary");
+
+            return MvcHtmlString.Create(tag.ToString());
+        }
+
+    }
+}
